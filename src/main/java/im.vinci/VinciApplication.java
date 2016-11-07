507f3b8518b2c0da0b722a7b77cc548a3f639eb0
mlc0202@126.com <malongchao@vinci.im>
2016-11-07 15:13:36 +0800

    提交缺失文件

--- src/main/java/im.vinci/VinciApplication.java
+++ src/main/java/im.vinci/VinciApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
         CacheConfiguration.class,
         ControllerConfiguration.class,
         ServiceConfiguration.class,
+        DaoConfiguration.class,
         ElasticsearchConfiguration.class,
 })
 public class VinciApplication {
